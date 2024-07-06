a86ea5eecf4554a2f5d9fd0041611f54f5b05b4f
Kim JiHyun <123639754+zomeong@users.noreply.github.com>
2024-07-07 00:06:41 +0900

    Title: 더 긴 문자열, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240707/더 긴 문자열/longer-string.py
@@ -0,0 +1,5 @@
+str1, str2 = input().split();
+
+if(len(str1) < len(str2)): print(str2, len(str2), sep = " ");
+elif (len(str1) > len(str2)): print(str1, len(str1), sep = " ");
+else: print("same");
